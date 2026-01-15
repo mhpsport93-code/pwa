d0ee8fb5738cef84061bde6422a901a35b81a0ef
mhpsport93-code <mhpsport93@gmail.com>
2026-01-15 18:56:33 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,16 @@
+// Install event
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch event (network first)
 self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {})
+  );
 });
